1d22c152eb342d2a773cbcec8a088eae6a0002d5
ricardonunosr <ricardonunosr@gmail.com>
2022-11-27 17:46:13 +0000

    Representing code

--- jlox/build.sh
+++ jlox/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 mkdir -p build
+
+# Generate Ast
+javac -d build com/craftinginterpreters/tool/GenerateAst.java
+java -cp build com.craftinginterpreters.tool.GenerateAst $@ com/craftinginterpreters/lox
+
 javac -d build com/craftinginterpreters/lox/Lox.java
 java -cp build com.craftinginterpreters.lox.Lox $@
